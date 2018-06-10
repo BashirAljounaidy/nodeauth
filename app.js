9dfc444acf381d4d339184482c1b31aa4be4d2d6
BashirAljounaidy <bma.963@gmail.com>
2018-06-10 14:09:52 +0200

    after work in good

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var passport = require('passport');
+var expressValidator = require('express-validator');
 var LocalStategy = require('passport-local').Strategy;
 var multer = require('multer');
+var upload = multer({dest:'./uploads'});
 var flash = require('connect-flash');
 var mongo = require('mongodb');
-var mongoose = require('mogoose');
+var mongoose = require('mongoose');
 var db = mongoose.connection;
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,16 +25,27 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-// Handel File upload
-app.use(multer({dest;'./uploads'}));
+// Handel File uploads
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//handel sessions
+app.use(session({
+    secret:'secret',
+    saveUninitialized:true,
+    resave:true
+}));
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+//validator
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,7 +64,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-Mak$@merna123456
-97097396
